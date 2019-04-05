041dd1647281a8e780771a31f354c1df7ab24be8
juncheng1011 <juncheng1011@gmail.com>
2019-04-05 18:47:35 +0800

    test

---
+++ src/main/java/com/ed/Hello.java
@@ -0,0 +1,15 @@
+package com.ed;
+
+public class Hello {
+    public static void main(String[] args) {
+        //System.out.println("Hello Java");
+        Person p = new Person();
+        p.hello();
+    }
+}
+
+//类别public class Hello {
+//main public static void main(String[] args) {
+//        System.out.println("Hello world");
+//    }
+//}
---
+++ src/main/java/com/ed/Person.java
@@ -0,0 +1,7 @@
+package com.ed;
+
+public class Person {
+    public void hello(){
+        System.out.println("Hello World");
+    }
+}
