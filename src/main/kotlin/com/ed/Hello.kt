96e51c43d518844d6fcb0355b59f8d6e18f67bd0
juncheng1011 <juncheng1011@gmail.com>
2019-04-05 19:32:42 +0800

    intital commit

--- src/main/kotlin/com/ed/Hello.kt
+++ src/main/kotlin/com/ed/Hello.kt
@@ -4,11 +4,6 @@ fun main(args: Array<String>) {
 //    println("Hello kotlin")
 //    Human().hello()
     Human().hello()
-    var age = 19
-    age = 20
-    var weight = 66.5f
-    var name = String
-    name = "Edmund"
 }
 
 class Human {
